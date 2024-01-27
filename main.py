d63a2af0c818d07ba0aaabcce7585535c74a047c
Максим <maksimnikiforov-1012567@mail.ru>
2024-01-27 19:56:37 +0900

    2 commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from fighter import Fighter
 
 if __name__ == '__main__':
 
@@ -8,10 +9,22 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Fighting')
 
+    fon = pygame.image.load('data/bg.jpg').convert_alpha()
+    fon = pygame.transform.scale(fon, (1000, 600))
+    screen.blit(fon, (0, 0))
+
+    fighter_1 = Fighter(200, 420)
+    fighter_2 = Fighter(700, 420)
+
+
     running = True
     while running:
+
+        fighter_1.draw(screen)
+        fighter_2.draw(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        pygame.display.flip()
+        pygame.display.update()
