1f0ee79eb491bf75bb3697d2c04b9a492c45a4ce
Максим <maksimnikiforov-1012567@mail.ru>
2024-02-04 18:04:04 +0900

    9 commit

--- fighter.py
+++ fighter.py
@@ -39,16 +39,17 @@ attack_anim_left = [
     pygame.transform.scale(pygame.image.load("data/attack_left (3).png"), (200, 310))
 ]
 class Fighter():
-    def __init__(self, x, y, player, image):
+    def __init__(self, x, y, player, image, direction):
         self.pos = vec(x, y)
         self.jump = False
         self.vel_y = 0
-        self.direction = "RIGHT"
+        self.direction = direction
         self.move_frame = 0
         self.attack_frame = 0
         self.attacking = False
         self.health = 100
         self.player = player
+        self.alive = True
 
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (200, 310))
@@ -57,70 +58,66 @@ class Fighter():
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
-
-
-
-
     def move(self):
         SPEED = 10
         GRAVITY = 2
         dx = 0
         dy = 0
         key = pygame.key.get_pressed()
+        if self.alive == True:
+            if self.player == 1:
+                if key[pygame.K_a]:
+                    dx = -SPEED
+                if key[pygame.K_d]:
+                    dx = SPEED
+                if key[pygame.K_w] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
 
-        if self.player == 1:
-            if key[pygame.K_a]:
-                dx = -SPEED
-            if key[pygame.K_d]:
-                dx = SPEED
-            if key[pygame.K_w] and self.jump == False:
-                self.vel_y = -30
-                self.jump = True
+                self.vel_y += GRAVITY
+                dy += self.vel_y
 
-            self.vel_y += GRAVITY
-            dy += self.vel_y
+                if self.pos.x + dx < 0:
+                    dx = -self.pos.x
+                if self.pos.x + dx > 800:
+                    dx = 800 - self.pos.x
+                if self.pos.y + dy > 600 - 310:
+                    self.vel_y = 0
+                    self.jump = False
+                    dy = 600 - 310 - self.pos.y
 
-            if self.pos.x + dx < 0:
-                dx = -self.pos.x
-            if self.pos.x + dx > 800:
-                dx = 800 - self.pos.x
-            if self.pos.y + dy > 600 - 310:
-                self.vel_y = 0
-                self.jump = False
-                dy = 600 - 310 - self.pos.y
+                self.dx = dx
+                self.rect.x += dx
+                self.rect.y += dy
+                self.pos.x += dx
+                self.pos.y += dy
 
-            self.dx = dx
-            self.rect.x += dx
-            self.rect.y += dy
-            self.pos.x += dx
-            self.pos.y += dy
+            if self.player == 2:
+                if key[pygame.K_LEFT]:
+                    dx = -SPEED
+                if key[pygame.K_RIGHT]:
+                    dx = SPEED
+                if key[pygame.K_UP] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
 
-        if self.player == 2:
-            if key[pygame.K_LEFT]:
-                dx = -SPEED
-            if key[pygame.K_RIGHT]:
-                dx = SPEED
-            if key[pygame.K_UP] and self.jump == False:
-                self.vel_y = -30
-                self.jump = True
+                self.vel_y += GRAVITY
+                dy += self.vel_y
 
-            self.vel_y += GRAVITY
-            dy += self.vel_y
+                if self.pos.x + dx < 0:
+                    dx = -self.pos.x
+                if self.pos.x + dx > 800:
+                    dx = 800 - self.pos.x
+                if self.pos.y + dy > 600 - 310:
+                    self.vel_y = 0
+                    self.jump = False
+                    dy = 600 - 310 - self.pos.y
 
-            if self.pos.x + dx < 0:
-                dx = -self.pos.x
-            if self.pos.x + dx > 800:
-                dx = 800 - self.pos.x
-            if self.pos.y + dy > 600 - 310:
-                self.vel_y = 0
-                self.jump = False
-                dy = 600 - 310 - self.pos.y
-
-            self.dx = dx
-            self.pos.x += dx
-            self.pos.y += dy
-            self.rect.x += dx
-            self.rect.y += dy
+                self.dx = dx
+                self.pos.x += dx
+                self.pos.y += dy
+                self.rect.x += dx
+                self.rect.y += dy
 
 
     def walk_anim(self):
@@ -152,14 +149,15 @@ class Fighter():
                 attacking_rect = pygame.Rect(self.pos.x - 100, self.pos.y, 100, 310)
             elif self.direction == 'LEFT':
                 attacking_rect = pygame.Rect(self.pos.x + 200, self.pos.y, 100, 310)
-            pygame.draw.rect(surface, (255, 255, 0), attacking_rect)
+            # pygame.draw.rect(surface, (255, 255, 0), attacking_rect)
             if attacking_rect.colliderect(target.rect):
                 target.health -= 1
-
-
+                if target.health < 0:
+                    target.alive = False
+                    image = pygame.image.load("data/Dead-ezgif.com-crop.png")
+                    target.image = pygame.transform.scale(image, (200, 310))
         else:
             pass
-
 
     def update(self, target, surface):
         self.attack(target, surface)
@@ -167,7 +165,7 @@ class Fighter():
         self.move()
 
     def render(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        # pygame.draw.rect(surface, (255, 0, 0), self.rect)
         surface.blit(self.image, self.pos)
 
 
--- main.py
+++ main.py
@@ -18,15 +18,15 @@ cursor = pygame.transform.scale(cursor, (32, 32))
 pygame.mouse.set_visible(False)
 
 def draw_bg():
-    fon = pygame.image.load('data/img.png')
+    fon = pygame.image.load('data/fon.jpg')
     fon = pygame.transform.scale(fon, (1000, 600))
     screen.blit(fon, (0, 0))
 
-# def healthbar(pos_x, pos_y, health):
-#     healthbar_rect = pygame.Rect((pos_x, pos_y, 400 * (health / 100), 30))
-#     health_rect = pygame.Rect((pos_x, pos_y, 400, 30))
-#     pygame.draw.rect(screen, (255, 0, 0), health_rect)
-#     pygame.draw.rect(screen, (255, 255, 0), healthbar_rect)
+def healthbar(pos_x, pos_y, health):
+    healthbar_rect = pygame.Rect((pos_x, pos_y, 400 * (health / 100), 30))
+    health_rect = pygame.Rect((pos_x, pos_y, 400, 30))
+    pygame.draw.rect(screen, (255, 0, 0), health_rect)
+    pygame.draw.rect(screen, (255, 255, 0), healthbar_rect)
 
 def main_menu():
     button_start = Button(width / 2 - (270 / 2), 250, 270, 74, "", "data/play01.png", "data/play02.png")
@@ -61,8 +61,13 @@ def main_menu():
         pygame.display.update()
 def new_game():
     running = True
-    fighter_1 = Fighter(200, 200, 1, "data/idle.png")
-    fighter_2 = Fighter(200, 200, 2, "data/idle.png")
+    fighter_1 = Fighter(200, 200, 1, "data/idle.png", "RIGHT")
+    fighter_2 = Fighter(600, 200, 2, "data/idle_l.png", "RIGHT")
+
+    round_end =  False
+    score = [0, 0]
+    round_end_time = 0
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -72,7 +77,33 @@ def new_game():
                 if event.key == pygame.K_s:
                     fighter_1.attacking = True
                     fighter_1.attack(fighter_2, screen)
+                if event.key == pygame.K_DOWN:
+                    fighter_2.attacking = True
+                    fighter_2.attack(fighter_2, screen)
 
+        if round_end == False:
+            if fighter_1.alive == False:
+                score[1] += 1
+                round_end = True
+                round_end_time = pygame.time.get_ticks()
+                print(score)
+            elif fighter_2.alive == False:
+                score[0] += 1
+                round_end = True
+                round_end_time = pygame.time.get_ticks()
+                print(score)
+        else:
+
+            if pygame.time.get_ticks() - round_end_time > 3000:
+                round_end = False
+                new_game()
+
+        if score[0] > 0 or score[1] > 0:
+            font = pygame.font.Font(None, 64)
+            text1 = font.render(f'VICTORY Player: {score.index(max(score))+1}', True, (150, 0, 0))
+        else:
+            font = pygame.font.Font(None, 0)
+            text1 = font.render("123", True, (255, 0, 0))
 
         fighter_1.move()
         fighter_2.move()
@@ -81,11 +112,12 @@ def new_game():
         fighter_2.render(screen)
         fighter_1.update(fighter_2, screen)
         fighter_2.update(fighter_1, screen)
-
+        healthbar(20, 20, fighter_1.health)
+        healthbar(580, 20, fighter_2.health)
+        screen.blit(text1, (300, 200))
 
         pygame.display.update()
         clock.tick(FPS)
-
 
 if __name__ == '__main__':
     main_menu()
